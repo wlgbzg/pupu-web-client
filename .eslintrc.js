109999fba58277b49596ea03570762be2b815fa6
sxs163mail <sxs163mail@gmail.com>
2023-05-25 18:05:58 +0800

    代码格式化

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    },
+	"scripts": {
+		"lint": "eslint --ext .js,.vue --ignore-path .gitignore --fix src",
+		"format": "prettier .  --write"
+	}
+	
+}
